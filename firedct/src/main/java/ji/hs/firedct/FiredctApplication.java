b3aba4b13831f14fca21376a4724438dbf43e1d5
now2w <now2w@DESKTOP-O2VVPQP>
2022-01-22 13:06:25 +0900

    first commit

---
+++ firedct/src/main/java/ji/hs/firedct/FiredctApplication.java
@@ -0,0 +1,13 @@
+package ji.hs.firedct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiredctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiredctApplication.class, args);
+	}
+
+}
---
+++ firedct/src/test/java/ji/hs/firedct/FiredctApplicationTests.java
@@ -0,0 +1,13 @@
+package ji.hs.firedct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiredctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
