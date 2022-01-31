e1d19566f7912dd56fcb3714e088d212ff7ec2d0
now2w <now2w@DESKTOP-O2VVPQP>
2022-01-31 15:51:18 +0900

    commit

--- firedct/src/main/java/ji/hs/firedct/co/Constant.java
+++ firedct/src/main/java/ji/hs/firedct/co/Constant.java
@@ -12,11 +12,6 @@ public class Constant {
 	public static final String KRX_JSON_URL = "http://data.krx.co.kr/comm/bldAttendant/getJsonData.cmd";
 	
 	/**
-	 * DART API KEY
-	 */
-	public static final String DART_API_KEY = "8425161daf2abf59fd7371874ce7aae1ec041ce9";
-	
-	/**
 	 * DART 종목코드 파일 URL
 	 */
 	public static final String DART_CORP_CD_URL = "https://opendart.fss.or.kr/api/corpCode.xml";
